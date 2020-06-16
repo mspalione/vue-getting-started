d7d14feb0e19cbc91c7011d871939faec5f6f16e
Melissa Spalione <mspalione@strategicmarketing.com>
2020-06-16 11:42:56 -0400

    routing

---
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import PageNotFound from './views/page-not-found';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            redirect: '/heroes',
+        }, {
+            path: '/heroes',
+            name: 'heroes',
+            //component: Heroes,
+            component: () =>
+                import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
+        },
+        {
+            path: '/heroes/:id',
+            name: 'hero-detail',
+            //component: HeroDetail,
+            component: () =>
+                import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+            props: r => ({id: parseInt(r.params.id)}),
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import(/* webpackChunkName: "bundle-about" */ './views/about.vue'),
+        },
+        {
+            path: '*',
+            component: PageNotFound,
+        },
+    ],
+});
